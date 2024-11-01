61d09e8e2c5933d07ac85216214ea1c4a7921f91
erickrocha87 <ericksaggi31@gmail.com>
2024-11-01 14:55:28 -0300

    Creating project and git repository

---
+++ src/db/DbExeception.java
@@ -0,0 +1,8 @@
+package db;
+
+public class DbExeception extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+    public DbExeception(String msg){
+        super(msg);
+    }
+}

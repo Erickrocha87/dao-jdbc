c929587a48ec435532f86a1c18aa2868f8e8f5b1
erickrocha87 <ericksaggi31@gmail.com>
2024-11-03 22:15:02 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -16,7 +16,7 @@ public class SellerDaoJDBC implements SellerDao {
 
     private Connection conn;
 
-    public SellerDaoJDBC(Connection conn){
+    public SellerDaoJDBC(Connection conn) {
         this.conn = conn;
     }
 
@@ -40,35 +40,44 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-       PreparedStatement st = null;
-       ResultSet rs = null;
-       try{
-        st = conn.prepareStatement("SELECT seller.*, department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.ID WHERE seller.Id = ? ");
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*, department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.ID WHERE seller.Id = ? ");
 
-        st.setInt(1, id);
-        rs = st.executeQuery();
-        if (rs.next()) {
-            Department dep = new Department();
-            dep.setId(rs.getInt("DepartmentId"));
-            dep.setName(rs.getString("DepName"));
-            Seller obj = new Seller();
-            obj.setId(rs.getInt("Id"));
-            obj.setName(rs.getString("Name"));
-            obj.setEmail(rs.getString("Email"));
-            obj.setBaseSalary(rs.getDouble("BaseSalary"));
-            obj.setBithDate(rs.getDate("BirthDate"));
-            obj.setDepartment(dep);
-            return obj;
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs, dep);
+                return obj;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbExeception(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
         }
-        return null;
-       }
-       catch(SQLException e){
-        throw new DbExeception(e.getMessage());
-       }
-       finally{
-        DB.closeStatement(st);
-        DB.closeResultSet(rs);
-       }
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBithDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
     }
 
     @Override
@@ -76,5 +85,6 @@ public class SellerDaoJDBC implements SellerDao {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findAll'");
     }
- //implementação da interface SellerDao em uma classe onde vai ser utilizado a tecnologia JDBC
+    // implementação da interface SellerDao em uma classe onde vai ser utilizado a
+    // tecnologia JDBC
 }

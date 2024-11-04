466ef9db0994127101e7e04cc75cc40269f5b47b
erickrocha87 <ericksaggi31@gmail.com>
2024-11-04 15:44:46 -0300

    insert DepartmentDao implements

--- src/aplication/Program2.java
+++ src/aplication/Program2.java
@@ -1,5 +1,24 @@
 package aplication;
 
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== TEST 1: Department insert ====\n");
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao(); //criando o DAO de department
+
+        System.out.println("Enter department name: ");
+        String name = sc.nextLine();
+        Department dep = new Department(null, name);
+        
+        departmentDao.insert(dep);
+        System.out.println("Inserted! New id = " + dep.getId());
+    }   
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,14 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import db.DB;
+import db.DbExeception;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -16,8 +22,34 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
     @Override
     public void insert(Department obj) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+       PreparedStatement st = null;
+
+       try{
+
+            st = conn.prepareStatement("INSERT INTO department (Name) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+
+            st.setString( 1, obj.getName());
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0 ) {
+                ResultSet rs = st.getGeneratedKeys();
+                if (rs.next()) {                            //inserindo (set) o id no objeto department criado, pegando a chave gerada pelo statement.RETURN_GENERATED_KEYS
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            }else{
+                throw new DbExeception("Unexpected error! No rows affected!");
+            }
+       }
+       catch(SQLException e){
+
+        throw new DbExeception(e.getMessage());
+
+       }
+       finally{
+        DB.closeStatement(st);
+       }
     }
 
     @Override

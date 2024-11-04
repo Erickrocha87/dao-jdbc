ba53a36ec1228036a663a0a2b191e97ac36253aa
erickrocha87 <ericksaggi31@gmail.com>
2024-11-04 00:38:08 -0300

    FindAll Implementation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -26,5 +26,12 @@ public class Program {
                 System.out.println(objSeller);
                 System.out.println();
             }
+
+            System.out.println("=== TEST 3: seller findAll ====\n");
+            list = sellerDao.findAll();
+            for (Seller obj : list) {
+                System.out.println(obj);
+                System.out.println("");
+            }
         }
 }
